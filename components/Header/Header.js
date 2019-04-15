a18a2e6f6922b29a53e0add52d9457f0ddcbb6b3
joerlop <jj.erlich155@gmail.com>
2019-04-14 19:41:18 -0500

    Finished header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,30 @@
 const title = document.querySelector(".header-left-content h1");
+const subtitle = document.querySelector(".header-left-content h2");
+const titleImg = document.querySelector(".header-right-content img");
 
-const timeTitle = 500;
+
+const timeTitle = 250;
 
 window.addEventListener("load", event => {
     if(mobile.matches) {
         title.classList.replace("hide-header", "show-navigation");
     } else {
-        setTimeout(() => title.classList.replace("hide-header", "fade-in-top"), timeTitle)   
+        setTimeout(() => title.classList.replace("hide-header", "fade-in-top"), timeTitle)
+    }
+})
+
+window.addEventListener("load", event => {
+    if(mobile.matches) {
+        subtitle.classList.replace("hide-header", "show-navigation");
+    } else {
+        setTimeout(() => subtitle.classList.replace("hide-header", "fade-in-top"), timeTitle)
+    }
+})
+
+window.addEventListener("load", event => {
+    if(mobile.matches) {
+        titleImg.classList.replace("hide-header", "show-navigation");
+    } else {
+        setTimeout(() => titleImg.classList.replace("hide-header", "flip-in-ver-left"), timeTitle + 500)
     }
 })
